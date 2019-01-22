f778fdbc9ec4d397aaf759c26c4f9f94b2d65dfe
VOLUNTIS\willy.dongmo <willy.dongmo@voluntis.com>
2019-01-22 14:10:58 +0100

    initial commit

---
+++ src/components/SearchBox.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SearchBox = ({ searchfield, searchChange }) => {
+  console.log('SearchBox');
+  return (
+    <div className='pa2'>
+      <input
+      id="search-robot"
+        aria-label="Search"
+        className='pa3 ba b--green bg-lightest-blue'
+        type='search'
+        placeholder='search robots'
+        onChange={searchChange}
+      />
+    </div>
+  );
+}
+
+export default SearchBox;
